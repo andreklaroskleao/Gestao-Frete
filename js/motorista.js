95d364a9f81544b8d1f07c0ae537cb2af16d3717
andreklaroskleao <andre.klar.leao@gmail.com>
2026-04-24 22:31:40 -0300

    Refactor logout handling in motorista.js

--- js/motorista.js
+++ js/motorista.js
@@ -1,17 +1,5 @@
-import { auth } from "./firebase.js";
 import { protegerPagina } from "./proteger.js";
-
-import {
-  signOut
-} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { configurarLogout } from "./logout.js";
 
 protegerPagina("motorista");
-
-const btnSair = document.getElementById("btnSair");
-
-if (btnSair) {
-  btnSair.addEventListener("click", async () => {
-    await signOut(auth);
-    window.location.href = "index.html";
-  });
-}
+configurarLogout();
